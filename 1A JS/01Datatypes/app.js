a6d76a07c31fda550bd4b856f966650dc366bcfc
Karan Singh <karans21.dev@gmail.com>
2025-02-01 20:31:56 +0530

    Undersing the concepts of JS Data types

---
+++ 1A JS/01Datatypes/app.js
@@ -0,0 +1,75 @@
+// Number
+let score = "48a";
+console.log(score);
+console.log(typeof score);
+console.log(typeof (score));
+
+let score2 = Number(score + 1);
+
+console.log(score2);
+console.log(typeof score2);
+
+// Boolean
+let isLoggedIn = false;
+let isBoolean = isLoggedIn;
+
+console.log(isBoolean);
+console.log(typeof isBoolean);
+
+String
+let str = 101;
+
+console.log(String(str));
+console.log(typeof String(str));
+
+//************ Operation**************
+
+let value = 30;
+let negValue = -value;
+console.log(negValue);
+console.log(typeof negValue);
+
+
+// String Concatination
+let f_Name = "Karan"
+let l_Name = "Singh"
+
+let fullName = f_Name + " " + l_Name;
+
+console.log(fullName);
+
+console.log(1 + "1");
+console.log("1" + 1);
+console.log("1" + 2 + 2); 
+console.log(1 + 2 + "2")
+
+console.log(true);
+console.log(false);
+console.log(+true);
+console.log(true+); // Wrong syntax
+console.log(+"");
+
+// Comparison of datatypes
+
+console.log("2" > 1);
+console.log("2a" > 1);
+
+console.log(null >  0);
+console.log(null ==  0);
+console.log(null >=  0);
+
+console.log(undefined >  0);
+console.log(undefined ==  0);
+console.log(undefined >=  0);
+
+// ===
+console.log("2" === 2);
+console.log("2" === "2");
+
+
+
+
+
+
+
+
