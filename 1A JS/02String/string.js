bb18c089b643d1263ef5a78b1470d51fe89f4f93
Karan Singh <karans21.dev@gmail.com>
2025-02-03 21:00:05 +0530

    half string methods are done

---
+++ 1A JS/02String/string.js
@@ -0,0 +1,22 @@
+const umane = "Karan"
+const repoCount = 100;
+//console.log(umane + repoCount +" repo count");  Outdated Concatination
+
+// Template literals or back ticks
+console.log(`Hello my name is ${umane} & my repo count is ${repoCount}`); // => String interpolation
+
+const str = new String("Karan");  //new way to write string
+console.log(str); // check the above string in web browser for different result
+console.log(str[0]); // find the vale of the index position
+console.log(str.__proto__); // to check the object in web browser
+console.log(str.length); // in-built function
+console.log(str.toUpperCase()); // these are the inbuld string function available in web browser
+console.log(str.charAt(2)); // Char at which position
+console.log(str.indexOf("a")); // Char at which position
+
+
+
+
+
+
+
