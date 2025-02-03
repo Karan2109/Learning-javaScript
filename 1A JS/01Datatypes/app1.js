d49d2e63e4abb05360f9c4d8b738cb67dacab3d3
Karan Singh <karans21.dev@gmail.com>
2025-02-03 14:52:37 +0530

    DataTypes

---
+++ 1A JS/01Datatypes/app1.js
@@ -0,0 +1,39 @@
+// Datatypes summary
+
+// Non-Premitive Datatypes
+// Array, Object, function
+
+//array
+const heros = ["Shaktiman","Krish", "DeadPool"];
+
+// object
+
+const hero = {
+  name: "Shaktiman",
+  age: 35,
+  powers: ["superhuman strength", "superhuman speed", "superhuman agility"],
+  isAlive: true,
+};
+
+// function
+const myFunction = function() {
+    console.log("Hello World!");
+};
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
+*/
