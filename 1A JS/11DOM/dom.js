c804ba00d2596a5dcddebc02b5e3cde05e55cdd1
Karan Singh <karans21.dev@gmail.com>
2025-02-12 20:41:51 +0530

    Completed with dom manipulation basics

---
+++ 1A JS/11DOM/dom.js
@@ -0,0 +1,21 @@
+// textContent: shows all the content inside the tag. Shoes hidden content also 
+// innerHTML: shows all the content inside the tag nut doesnt show the hidden text content
+// innerText: Give the enitre contentt with html content inclusive
+
+// document.getElementById("");
+// document.getElementsByClassName("");
+// document.getElementsByTagName("");
+// document.querySelector(""); // no need to give index
+// document.querySelectorAll(""); // need to give the index also to run the code
+
+
+// NodeList is not a pure array
+// use forEach loop for Nodelist to manipulate the web pages
+
+// HTML collection is not a pure array but array like collection
+
+
+// Array.from(); // pass the value to convert to a array (Array.from "A" will be capital)
+
+// const parent = document.querySelector(".parent");
+// console.log("parent");
