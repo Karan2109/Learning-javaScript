d956cfadda2376903d6c76252f9f962db53d6d8b
Karan Singh <karans21.dev@gmail.com>
2025-02-17 13:07:01 +0530

    How to take a reference from one function to another using .call & this object

---
+++ 1A JS/14Classes_&_OOPS/call.js
@@ -0,0 +1,16 @@
+function SetUsername(username) {
+    //Complex DB calls
+    this.username = username;
+}
+
+fuunction.createUser(username, email, password) {
+    SetUsername.call(this, username); // .call holds the reference of username from Setusername function
+    // the above this is taking reference from the above this function when use .call function use this object
+    
+    this.email = email;
+    this.password = password;
+}
+
+const chai = new createUser("chai","chai@gmail.com","123");
+
+console.log(chai); // Output: SetUsername { username: 'chai' }
